9a4bc7b53ad21f9e4626002a98e0c412d23f853c
madelinenlee <madelinenlee@gmail.com>
2019-02-06 19:13:21 -0500

    updated to provide only top 3 selling products

--- sales_reporting.py
+++ sales_reporting.py
@@ -6,18 +6,21 @@ Created on Tue Feb  5 18:10:35 2019
 @author: madeline
 """
 
-import os
-import csv
+#import os
+#import csv
 import pandas as pd
-import itertools as it
-import datetime
+#import itertools as it
+#import datetime
 import operator
+#from collections import OrderedDict
 
 monthly_sales_dataframe = pd.read_csv('/Users/madeline/Desktop/SPRING_2019/OPIM_243/sales-reporting-exercise/data/sales-201710.csv')
 attributes = monthly_sales_dataframe.columns.tolist()
-monthly_sales_dataframe['date'] = monthly_sales_dataframe['date'].to_datetime()
+monthly_sales_dataframe['date'] = pd.to_datetime(monthly_sales_dataframe['date'])
 
-print('SALES REPORT (', )
+print('SALES REPORT ('+ monthly_sales_dataframe['date'][0].strftime('%B') +
+                     ' ' + monthly_sales_dataframe['date'][0].strftime('%Y') +
+                     ')')
 print('TOTAL SALES : $', str('%0.2f'%monthly_sales_dataframe['sales price'].sum()))
 
 products = monthly_sales_dataframe['product'].unique().tolist()
@@ -32,8 +35,12 @@ for item in products:
 sorted_product_subtotals = sorted(product_subtotals.items(), key = operator.itemgetter(1))
 product_subtotals = dict(reversed(sorted_product_subtotals))
 
-print('TOP SELLING PRODUCTS: ')
+print('TOP 3 SELLING PRODUCTS: ')
 count = 1
 for item in product_subtotals:
     print(str(count) + '. ' + item + ' $' + str('%0.2f'%round(product_subtotals[item], 2)))
     count = count + 1
+    if count > 3:
+        break
+        
+
